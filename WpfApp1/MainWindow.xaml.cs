75420ec35ac6670f87a6d22562444465433d8bc8
eitay <eitay@soos.org.il>
2023-04-19 20:01:24 +0300

    init repo

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<student> students;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void showStudents_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addStudent_Click(object sender, RoutedEventArgs e)
+        {
+            FirstName.Visibility = Visibility.Visible;
+            FirstNameLabel.Visibility = Visibility.Visible;
+            LastName.Visibility = Visibility.Visible;
+            LastNameLabel.Visibility = Visibility.Visible;
+
+        }
+
+        private void addBulk_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void addOne_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApp1/student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class student
+    {
+        public string firstName;
+        public string lastName;
+        public string ID;
+        public string Email;
+        public string phone;
+        public int[] grades;
+        public double avargae;
+        public student (string firstName, string lastName, string ID, string Email, string phone, int[] grades)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.ID = ID;
+            this.Email = Email;
+            this.phone = phone;
+            this.grades = grades;
+            int count = 0;
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (grades[i] != 777)
+                {
+                    count++;
+                    this.avargae += grades[i];
+                }
+            }
+            this.avargae /= count;
+        }
+    }
+}
